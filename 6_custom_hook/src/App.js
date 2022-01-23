7ba6c6ee0036cc20b349e3588c3be431cd419642
nicolas <apple@Apples-MacBook-Air.local>
2022-01-23 16:36:41 +0800

    feat: edit root component

--- 6_custom_hook/src/App.js
+++ 6_custom_hook/src/App.js
@@ -1,20 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import Form from './components/Form';
+import background from './assets/background.jpg';
+
+const Root = styled.div`
+  height: 100vh;
+  background: url(${background}) no-repeat center center fixed; 
+`;
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+      <Form />
+    </Root>
   );
 }
 
